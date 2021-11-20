a29663d2ac31de222b356e459281fa2987af0966
unknown <easy>
2021-11-20 22:13:30 +0800

    feat: image

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -43,15 +43,29 @@ const modules = {
       exclude: /node_modules/,
     },
     //  // Images
-    //  {
-    //   test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-    //   type: 'asset/resource',
-    // },
+    {
+      // 问题：默认处理不了html中img图片
+      // 处理图片资源
+      test: /\.(jpg|png|gif|svg)$/,
+      // 使用一个loader
+      // 下载 url-loader file-loader
+      loader: 'url-loader',
+      options: {
+        // 图片大小小于8kb，就会被base64处理
+        // 优点: 减少请求数量（减轻服务器压力）
+        // 缺点：图片体积会更大（文件请求速度更慢）
+        limit: 8 * 1024,
+        // 问题：因为url-loader默认使用es6模块化解析，而html-loader引入图片是commonjs
+        // 解析时会出问题：[object Module]
+        // 解决：关闭url-loader的es6模块化，使用commonjs解析
+        esModule: false,
+        // 给图片进行重命名
+        // [hash:10]取图片的hash的前10位
+        // [ext]取文件原来扩展名
+        name: 'assets/images/[hash:10].[ext]',
+      },
+    },
     // // Fonts and SVGs
-    // {
-    //   test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-    //   type: 'asset/inline',
-    // },
     // CSS, PostCSS, and Sass
     {
       test: /\.(css)$/,
@@ -98,7 +112,7 @@ const modules = {
 
 const plugins = [
   new HtmlWebpackPlugin({
-    title: '写着玩',
+    title: 'chat',
     template: path.resolve(__dirname, '../src/index.html'),
     filename: 'index.html',
   }),
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from 'react'
 
-import * as styles from './index.scss'
+import cssExports from './index.scss'
 
 const Login: React.FC<any> = (props: any) => {
-  useEffect(()=>{
-    console.log(styles)
-  }, [])
 
   return (
-    <div className={styles.default.login}>
+    <div className={cssExports.login}>
       登录
     </div>
   )
